bed1fb31aa2f3155cdacb72f33a9dd645e7d87bc
godcandidate <godcandidate101@gmail.com>
2024-12-10 10:56:33 +0000

    feat: add Todo model

---
+++ models/todo.model.ts
@@ -0,0 +1,55 @@
+import mongoose, { Document, Model, Schema } from "mongoose";
+import 'dotenv/config';
+
+export interface ITodo extends Document {
+  title: string;
+  description: string;
+  priority: string;
+  category: string;
+  date: Date;
+  status: String;
+}
+
+// Todo Schema
+const todoSchema: Schema<ITodo> = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: [true, "Title is required"],
+    },
+    description: {
+      type: String,
+      required: [true, "Description is required"],
+    },
+    priority: {
+      type: String,
+      required: [true, "Priority is required"],
+      enum: ["low", "medium", "high"],
+    },
+    category: {
+      type: String,
+      required: [true, "Category is required"],
+      enum: ["work", "personal", "shopping", "other"],
+    },
+    date: {
+      type: Date,
+      required: [true, "Date is required"],
+    },
+    status: {
+      type: String,
+      default: "active",
+      enum: ["active", "completed"]
+    }
+  },
+  { timestamps: true }
+);
+
+// Set the current date to indicate completion
+todoSchema.methods.markCompleted = function (): void {
+  this.date = new Date(); 
+};
+
+// Todo Model
+const todoModel: Model<ITodo> = mongoose.model("Todo", todoSchema);
+
+export default todoModel;
